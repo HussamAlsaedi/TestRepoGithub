ba5fc3a01b7d8d764a20f75331f2773ee1297966
halsa <h.alsaedi1992@outlook.com>
2024-12-25 22:14:12 +0300

    added Model Customer, Service CustomerService

---
+++ src/main/java/com/example/demo/Service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.example.demo.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomerService {
+
+
+}
