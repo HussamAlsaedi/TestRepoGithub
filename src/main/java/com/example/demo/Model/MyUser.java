be0abc12a8adbbf4f0e99a5e6f7220a56774beca
halsa <h.alsaedi1992@outlook.com>
2024-12-25 21:49:20 +0300

    add Model MyUser

---
+++ src/main/java/com/example/demo/Model/MyUser.java
@@ -0,0 +1,17 @@
+package com.example.demo.Model;
+
+import jakarta.annotation.security.DenyAll;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MyUser {
+
+}
