fbc762d2055f11fb90ba6f4a829bde43354d80a2
halsa <h.alsaedi1992@outlook.com>
2024-12-26 18:27:58 +0300

    add Product Model

--- src/main/java/com/example/demo/Model/Admin.java
+++ src/main/java/com/example/demo/Model/Admin.java
@@ -2,6 +2,8 @@ package com.example.demo.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +21,7 @@ public class Admin {
     @Id
     private Long id;
 
+    @NotEmpty(message = "name is mandatory")
+    @Size(min = 2,max = 10,message = "name characters must be more then 1.")
     private String name;
 }
--- src/main/java/com/example/demo/Model/Product.java
+++ src/main/java/com/example/demo/Model/Product.java
@@ -1,23 +1,15 @@
 package com.example.demo.Model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.*;
 
-@Setter @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-
-public class Customer {
+public class Product {
 
     @Id
-    private Long id;
+    private Integer employeeID;
 
     @NotEmpty(message = "name is mandatory")
     @Size(min = 2,max = 10,message = "name characters must be more then 1.")
     private String name;
-
 }
